fea3789f8d664a37d1cae9dec1f13f2f338c2f97
Guilherme Luiz Sampaio <64015265+Guilherme-Sampaio@users.noreply.github.com>
2023-11-21 23:28:04 +0000

    add missed files

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import './styles.css'
+
+function Home() {
+    return <div className='container'>
+        <h1 className='text'>Oi mamãe! Eu e papai amamos você!</h1>
+        <img className='image' src='photo-together.jpeg'/>
+        <h1 className='text'>Assinado: Oliver</h1>
+        <img className='image' src='photo-solo.jpeg'/>
+    </div>
+}
+
+export default Home;
